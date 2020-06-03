96a8eb534431835a2243a533725926a4f753d85e
AshwinDV <ashwindv75@gmail.com>
2020-06-03 20:16:47 +0530

    Edit requirements

--- basic_imports.py
+++ basic_imports.py
@@ -1,8 +1,6 @@
 #import required modules
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import datetime
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
